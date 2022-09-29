362f3101f8b4883976306536e29c6de527e353fc
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-09-29 17:44:25 -0600

    Update variables.py

--- Assignment2/variables.py
+++ Assignment2/variables.py
@@ -10,7 +10,6 @@ print(letter2)
 print(letter3)
 print(letter4)
 print(letter5)
-
 3)
 letter1 = 'M'
 letterX = 'M'
@@ -18,9 +17,7 @@ letterX = 'M'
 print(letter1)
 print(letterX)
 ## 'M' was successfully printed with both variables under the same command line.
-
 5)
-
 letter1 = 'M'
 letter2 = 'E'
 letter3 = 'R'
@@ -35,7 +32,6 @@ print(letter4)
 print(letter5)
 print(letterX)
 ## Changing the value of X did not change the value of other variables.
-
 6)
 letter1 = 'M'
 letterX = letter1
@@ -43,12 +39,11 @@ letterX = letter1
 print(letterX)
 print(letter1)
 ## 'M' is print from both variables.
-
 letter1 = 'Z'
 letterX = letter1
 
 print(letterX)
 print(letter1)
-## letter1 new values changes letterX's values as well. This means 
+## letter1 new value changes letterX's value as well. This means 
 ## that we can use variables to store info to reference items within
-## different areas of you program.
+## different areas of our program.
