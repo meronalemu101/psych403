79490e2dc6e725e683c147b53fe0b4bcf48e3f2f
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-09-29 17:09:13 -0600

    Update scratch.py

--- Assignment2/scratch.py
+++ Assignment2/scratch.py
@@ -1,3 +1,15 @@
 print (5/2)
 print (5.0/2.0)
 ## Both scripts gave the answer "2.5"
+print (7%5)
+print (16%4)
+## % is the modulus tool which finds the remainder of a and b or a%b
+print (3**4)
+print (7**5)
+## ** is the power tool which give you the power of a to b when a**b
+print (8//4)
+print (13//6)
+## // is the integer division tool which divides a by b in a//b, and 
+## removes existing remainders. The answers to both scripts is 2.
+print (3+4-6*8/2)
+## script printed -17. This answer does follow the order of operations.
