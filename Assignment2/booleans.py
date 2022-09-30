8d184774a8a68c3c70bab36b0d39483af5c77372
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-09-29 18:21:16 -0600

    Update booleans.py

--- Assignment2/booleans.py
+++ Assignment2/booleans.py
@@ -1,1 +1,19 @@
+1)
 print(1==1.0)
+## script printed 'True' so these equal eachother. 
+print("1"=="1.0")
+## scripted printed 'False', so number presented aren't equal to eachother. 
+## Adding "" makes python recgonizes the numbers as its own string and 
+## not identical values.
+
+2)
+print(5 == (3+2))
+## script printed True. These values are the same.
+
+3)
+print(1 == 1.0) and ("1" == "1.0") and (5 == (3+2))
+## scripts prints 'True'.
+print(1 == 1.0) or ("1" == "1.0") or (5 == (3+2))
+## script prints 'True'.
+print(1 == 1.0) and not ("1" == "1.0") and not (5 == (3+2))
+## script prints 'True'.
