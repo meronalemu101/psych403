fbdb62aac3db0035a25a722219d637af32524c9b
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-09-29 18:28:19 -0600

    Add files via upload

---
+++ Assignment2/lists.py
@@ -0,0 +1,9 @@
+oddlist = [1, 3, 5, 7, 9]
+subject_names = ['sub1', 'sub2', 'sub3', 'sub4', 'sub5']
+responses = [3, 'yes', '1.0', True]
+exp = 'my_exp'
+
+print(oddlist)
+print(subject_names)
+print(responses)
+print(exp)
