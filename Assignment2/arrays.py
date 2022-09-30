8247c11ca793f17f1cb75d77a55b0cbb3976af8f
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-09-29 19:25:51 -0600

    Update arrays.py

--- Assignment2/arrays.py
+++ Assignment2/arrays.py
@@ -1,4 +1,17 @@
+1)
 import mixnums
 data = mixnums.array([1, 2, 3, 3.1, 5.3, 4.6])
 print(data)
-print(type(data))
+
+2)
+import mixtypes
+data = mixtypes.array([7, 13, 33.6, 51.9, "mine", "yours"])
+print(data)
+
+3)
+import oddarray
+data = oddarray.array(list(range(100))
+print(data)
+
+4)
+## Difficulty understanding how to do logs within script. Pardon me.
