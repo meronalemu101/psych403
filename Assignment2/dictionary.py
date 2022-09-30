1c58702baf82841bd9d1acf6cb01d2491d41592b
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-09-29 18:55:36 -0600

    Add files via upload

---
+++ Assignment2/dictionary.py
@@ -0,0 +1,7 @@
+my_name = "MERON"
+my_age = [23.0]
+year_study = [4]
+favourite_food = ["tacos", "ribs", "fries", "chocolate milk"]
+
+about_me = {'myname': my_name, 'myage': my_age, 'yearofstudy': year_study, 'favouritefood': favourite_food}
+print(about_me)
