506299bdbe43328b45db33f155d9704f8ba75034
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-09-29 19:00:26 -0600

    Update dictionary.py

--- Assignment2/dictionary.py
+++ Assignment2/dictionary.py
@@ -1,3 +1,4 @@
+1&2)
 my_name = "MERON"
 my_age = [23.0]
 year_study = [4]
@@ -5,3 +6,10 @@ favourite_food = ["tacos", "ribs", "fries", "chocolate milk"]
 
 about_me = {'myname': my_name, 'myage': my_age, 'yearofstudy': year_study, 'favouritefood': favourite_food}
 print(about_me)
+## script printed with no errors.
+print(type(about_me))
+## classified script type as 'dict'. Dictionary correctly made.
+3)
+print(len(about_me))
+## length printed is 4. Recgonized each of the 4 items listed for myself
+## because of the commas separating them in the about_me script.
