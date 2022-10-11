8613df14b49a55638c2bee8407679eba91226927
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-10-11 16:47:51 -0600

    Update variables.py

--- Assignment3/variables.py
+++ Assignment3/variables.py
@@ -1,1 +1,12 @@
+1) #"sub_str" is used to get the answer 'sub2' because using the "sub_int" 
+#variable will just give the answer 'subsub' because the integer is
+#adding the previous variable by 2. 
 
+2) print(sub_code + " " + sub_str)
+#answer is 'sub 2'
+print(sub_code + " " + sub_str + sub_str + sub_str)
+#answer is 'sub 222'
+print((sub_code + sub_str)*3)
+#answer is sub2sub2sub2
+print(sub_code + sub_code + sub_code + sub_str + sub_str + sub_str)
+#answer is subsubsub222
