9752758502012ea589a149b945d974746a3c7174
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-10-11 18:58:55 -0600

    Create indexing.py

---
+++ Assignment3/indexing.py
@@ -0,0 +1,14 @@
+1)
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+2)
+print(colors[4])
+#answer given: blue
+3)
+print(colors[4][2], colors[4][3])
+#answer given: e u
+4)
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+colors.remove('purple')
+colors.insert(6, 'indigo' + " " + 'violet')
+print(colors)
+#answer given: ['red', 'orange', 'yellow', 'green', 'blue', 'indigo violet']
