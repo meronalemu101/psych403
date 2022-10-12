d64c95d39fac707b19bd682413aa111665acdfae
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-10-11 18:08:46 -0600

    Update listoperations.py

--- Assignment3/listoperations.py
+++ Assignment3/listoperations.py
@@ -1,1 +1,18 @@
+1) 
+numlist = [1, 2, 3]
+print(numlist * 2)
+#answer given: '[1, 2, 3, 1, 2, 3]'
+2)
+#I need to come in for office hours to workout numpy.array on PyCharm 
+#please and thank you.
+3)
+#could not code
 
+print(strlist * 2)
+#answer: ['do', 're', 'mi', 'fa', 'do', 're', 'mi', 'fa']
+
+#could not code
+      
+strlist = [['do']*2, ['re']*2, ['mi']*2, ['fa']*2]
+print(strlist)
+#answer: [['do', 'do'], ['re', 're'], ['mi', 'mi'], ['fa', 'fa']]
